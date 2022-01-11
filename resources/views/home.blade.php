1f601354f2e4ec8eca2104ae954e29bfe584eed3
Mattia311 <mattia.space98@gmail.com>
2022-01-11 16:39:57 +0100

    Esercizio completo più bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,18 @@
 
 
 @section('content')
-<h1>ciao</h1>
+<h1 class="text-center">Questi sono tutti i film:</h1>
+<div class="container text-center">
+    @foreach ($movies as $movie)
+    <div class="card m-3">
+        <h2>Titolo: {{$movie['title']}}</h2>
+        <h3>Titolo Originale: {{$movie['original_title']}}</h3>
+        <h4>Nazionalità: {{$movie['nationality']}}</>
+            <h4>Data di uscita: {{$movie['date']}}</h4>
+            <h4>Voto della critica: {{$movie['vote']}}</h4>
+    </div>
+    @endforeach
+</div>
 
 
 @endsection
