000d524ca89606fa9d4f023d0c17eeb854b46af4
Mattia311 <mattia.space98@gmail.com>
2022-01-11 16:23:16 +0100

    Using ddd

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        ddd(Movie::all());
+        return view('home');
+    }
 }
